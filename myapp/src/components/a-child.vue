cb822a9866466a49adba08f2f814391ec136e111
YYB <1755916756@qq.com>
2020-10-10 14:14:08 +0800

    vue-cli components

---
+++ myapp/src/components/a-child.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h3>这是子组件a的子界面</h3>
+  </div>
+</template>
+<script>
+export default {
+    name:'achild'
+}
+</script>
--- myapp/src/components/a.vue
+++ myapp/src/components/a.vue
@@ -1,11 +1,14 @@
+
 <template>
   <div>
     <a>这是a界面</a>
     <button @click="toB">去往B界面query</button>
     <button @click="toBpa">去往B界面paramas</button>
+    <achild></achild>
   </div>
 </template>
 <script>
+import achild from './a-child.vue'
 export default {
   data () {
     return {}
@@ -19,6 +22,9 @@ export default {
       // 类似于post请求
       this.$router.push({ name: 'b', params: { name: 'YYBLOVE', age: 122 } })
     }
+  },
+  components:{
+    'achild':achild
   }
 }
 </script>
