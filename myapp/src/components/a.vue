48d969fd66a9a061f481ef5863bfa4cf46a6f578
YYB <1755916756@qq.com>
2020-10-10 09:39:55 +0800

    路由

--- myapp/src/components/a.vue
+++ myapp/src/components/a.vue
@@ -1,5 +1,18 @@
 <template>
   <div>
     <a>这是a界面</a>
+    <button @click="toB">去往B界面</button>
   </div>
 </template>
+<script>
+export default {
+  data () {
+    return {}
+  },
+  methods: {
+    toB () {
+      this.$router.push({ path: '/b', query: { name: 'YYB', age: 12 } })
+    }
+  }
+}
+</script>
--- myapp/src/components/b.vue
+++ myapp/src/components/b.vue
@@ -1,5 +1,25 @@
 <template>
-    <div>
-        <a>这是b界面</a>
-    </div>
+  <div>
+    <a>这是b界面</a>
+    <!--  -->
+    <button @click="toA">去往A界面</button>
+    <button @click="getA">getData</button>
+  </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {}
+  },
+  methods: {
+    toA () {
+      console.log(this.$router)
+      this.$router.push({ path: '/a' })
+    },
+    getA () {
+      console.log(this.$route.query)
+    }
+  }
+}
+</script>
